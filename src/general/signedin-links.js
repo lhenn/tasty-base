4f6848354cdfa6b8acd9ef65f6991c67ab4d0e8a
laura <hennlaura1@gmail.com>
2020-05-03 14:11:10 +0200

    changed account styling

--- src/general/signedin-links.js
+++ src/general/signedin-links.js
@@ -1,34 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { getFirebase } from "../firebase";
 import Button from "./button-primary";
 
-const UserIcon = styled.div`
+const UserWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction:column;
+  align-items:center;
 `;
 const UserPhoto = styled.img`
-  width: 50px;
+  width: 42px;
+  height: 42px;
   border-radius: 50px;
+`;
+const SignOutLink = styled.button`
+  background-color:inherit;
+  padding-top:10px;
+  color:white;
+  border:none;
+  &:hover{
+    color:blue;
+    cursor:pointer;
+  }
 `;
 
 const SignedInLinks = ({ user }) => {
+  const [showUserOptions, setShowUserOptions] = useState(false);
   // TODO: useCallback?
   const logout = () => {
     getFirebase().auth().signOut();
   };
+
+  
 
   return (
     <>
       <Link to="/create">
         <Button>Create a post</Button>
       </Link>
-      <UserIcon>
+      <UserWrapper
+        onClick={() => setShowUserOptions(true)}
+      >
         <UserPhoto src={user.photoURL} />
-        {user.displayName}
-      </UserIcon>
-      <Button onClick={logout}>Sign out</Button>
+        <SignOutLink onClick={logout}>Sign out</SignOutLink>
+      </UserWrapper>
     </>
   );
 };
