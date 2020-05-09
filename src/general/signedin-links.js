605215020ca9b1264b79ec51ebb2c092722f4a21
Adam Coogan <dr.adam.coogan@gmail.com>
2020-05-09 23:39:16 +0200

    improve signout tooltip

--- src/general/signedin-links.js
+++ src/general/signedin-links.js
@@ -1,13 +1,13 @@
-import React, { useState, useRef } from "react";
-import { Link , Redirect} from "react-router-dom";
+import { faBookmark, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import { Link, Redirect } from "react-router-dom";
 import styled from "styled-components";
 import { getFirebase } from "../firebase";
 import PrimaryButton from "./button-primary";
 import NavItem from "./nav-item";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBookmark, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
-import Overlay from "react-bootstrap/Overlay";
-import Tooltip from "react-bootstrap/Tooltip";
 
 const UserWrapper = styled.div`
   display: flex;
@@ -23,16 +23,17 @@ const UserPhotoC = styled.img`
     cursor: pointer;
   }
 `;
+
 const TtInner = styled.div`
   display: flex;
   flex-direction: column;
   padding: 10px 0;
-  
 `;
+
 const SignOutLink = styled.button`
   background-color: inherit;
   padding-top: 10px;
-  color:#dedddd;
+  color: #dedddd;
   border: none;
   &:hover {
     color: white;
@@ -40,36 +41,40 @@ const SignOutLink = styled.button`
   }
 `;
 
-const UserPhoto = ({ user }) => {
-  const [show, setShow] = useState(false);
-  const target = useRef(null);
-  const logout = () => {
-    getFirebase().auth().signOut().then(function() {
-      return(
-        <Redirect to="/"/>
-      )
-    }, function(error) {
-      console.error('Sign Out Error', error);
-    });;
-  };
+const logout = () => {
+  getFirebase()
+    .auth()
+    .signOut()
+    .then(
+      function () {
+        return <Redirect to="/" />;
+      },
+      function (error) {
+        console.error("Sign Out Error", error);
+      }
+    );
+};
 
+const UserPhoto = ({ user }) => {
   return (
     <>
-      <UserPhotoC
-        src={user.photoURL}
-        ref={target}
-        onClick={() => setShow(!show)}
-      />
-      <Overlay target={target.current} show={show} placement="bottom">
-        {(props) => (
-          <Tooltip id="overlay" {...props}>
+      <OverlayTrigger
+        placement="bottom"
+        trigger="click"
+        overlay={
+          <Tooltip id="overlay">
             <TtInner>
               <strong>{user.displayName}</strong>
-              <SignOutLink onClick={logout}>Sign out <FontAwesomeIcon icon={faSignOutAlt}/></SignOutLink>
+              <SignOutLink onClick={logout}>
+                Sign out <FontAwesomeIcon icon={faSignOutAlt} />
+              </SignOutLink>
             </TtInner>
           </Tooltip>
-        )}
-      </Overlay>
+        }
+        rootClose
+      >
+        <UserPhotoC src={user.photoURL} alt="user photo" />
+      </OverlayTrigger>
     </>
   );
 };
