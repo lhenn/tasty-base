1321d6afd92b715dbdd9969a5d9eb46c4ce3be35
laura <hennlaura1@gmail.com>
2020-05-30 15:48:52 +0200

    active icon fix

--- src/recipes/star.js
+++ src/recipes/star.js
@@ -48,6 +48,7 @@ const Star = ({ slug }) => {
   const onMouseEnter = () => {
     !isStarred ? setTTText("Star") : setTTText("Unstar");
   };
+  console.log('isStarred:', isStarred)
 
   return (
     <OverlayTrigger
@@ -55,7 +56,7 @@ const Star = ({ slug }) => {
       trigger={["hover", "focus"]}
       overlay={<UpdatingTooltip id="star-tooltip">{ttText}</UpdatingTooltip>}
     >
-      <Icon icon={faStar} isActive={isStarred} onClick={onClick} onMouseEnter={onMouseEnter} />
+      <Icon icon={faStar} isactive={isStarred ? 1 : 0} onClick={onClick} onMouseEnter={onMouseEnter} />
     </OverlayTrigger>
   );
 };
