db290ff4fa478bb54b24ef9bef619eabe7033c5d
laura <hennlaura1@gmail.com>
2020-06-01 15:09:15 +0200

    loading all mylist posts on page now

--- src/pages/mylist.js
+++ src/pages/mylist.js
@@ -1,8 +1,3 @@
-// import React, { useContext, useState } from 'react';
-// import { fetchSortedPosts, getFirebase } from "../firebase";
-
-// import { UserContext } from "../App";
-// import {fetchMyListPosts} from "../firebase";
 import React, { useContext, useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
 import { UserContext } from "../App";
@@ -29,14 +24,12 @@ const MyList = () => {
 
   // Load all posts if userData is present
   useEffect(() => {
-    if (!loadingUserData && userData?.starredRecipes && userData?.checkedRecipes) {
-    const myListSlugs = Object.keys(userData.starredRecipes).concat(Object.keys(userData.checkedRecipes));
-    console.log('myListSlugs', myListSlugs);
+    if (!loadingUserData && userData?.myListRecipes) {
       setPosts({ posts: [], loadingPosts: true });
       // Values are timestamps
-      addPromise(fetchPosts(Object.keys(userData.starredRecipes))).then(
-        (favPosts) => {
-          setPosts({ posts: favPosts, loadingPosts: false });
+      addPromise(fetchPosts(Object.keys(userData.myListRecipes))).then(
+        (myListPosts) => {
+          setPosts({ posts: myListPosts, loadingPosts: false });
         },
         (err) => console.log("Stars: fetchPosts failed: ", err)
       );
@@ -44,8 +37,7 @@ const MyList = () => {
   }, [userData, loadingUserData, addPromise]);
  
   // User auth and data loading underway
-  if (loadingUser || loadingUserData) return <p>loading user info</p>;
-  console.log(user, userData);
+  if (loadingUser || loadingUserData) return <p>loading...</p>;
 
   // User auth completed
   if (!user) {
@@ -53,12 +45,8 @@ const MyList = () => {
   }
 
   // User exists and may have favorite posts that are being loaded
-  if (loadingPosts) {
-    return <h1>Loading favorites recipes...</h1>;
-  }
-
-  // getFirebase().database().ref(`/posts`).once("value")
-  // .then((snapshot) => (console.log(snapshot.val())))
+  if (loadingPosts) return <p>loading...</p>;
+  
   return (
     <>
       <h1>MyList</h1>
