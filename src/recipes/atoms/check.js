54559e842e67de6ae81866b39b29350ae82ad4e1
Laura Henn <laura@relevantonline.eu>
2020-08-17 20:46:13 +0200

    tooltip closing on submit

--- src/recipes/atoms/check.js
+++ src/recipes/atoms/check.js
@@ -1,11 +1,13 @@
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef } from "react";
 import { UserContext } from "../../App";
 import { addToMyList, removeFromMyList } from "../../firebase";
 import { FormGroup, FormRow, Input, Label } from "../../forms/general-forms";
 import { PrimaryButton } from "../../general/buttons";
 import { Icon } from "./generic-icons";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Overlay from "react-bootstrap/Overlay";
+import Button from "react-bootstrap/Button";
 import Tooltip from "react-bootstrap/Tooltip";
 import styled from "styled-components";
 
@@ -14,8 +16,11 @@ const TtInner = styled.div`
   flex-direction: column;
   padding: 10px 0;
 `;
-const RateTT = (props) => {
+
+function RateToolTip(props) {
+  console.log("props:", props);
   const [show, setShow] = useState(false);
+  const target = useRef(null);
   const [ease, setEase] = useState(5);
   const [taste, setTaste] = useState(5);
   const { user } = useContext(UserContext);
@@ -23,7 +28,7 @@ const RateTT = (props) => {
   const handleSubmit = () => {
     sendRatings(ease, taste);
     setShow(false);
-  }
+  };
   const sendRatings = (ease, taste) => {
     addToMyList(user.uid, props.slug, "rate", { ease, taste })
       .then(() => {
@@ -31,57 +36,56 @@ const RateTT = (props) => {
       })
       .catch((err) => console.log(err));
   };
+  // Currently wrapping Icon in a Button because otherwise there is a "functional components not compatible with useRef error"
+  // TODO: improve the check clicking active/not active and checked/not checked behavior
   return (
     <>
-      <OverlayTrigger
-        onClick={() => setShow(!show)}
-        placement="bottom"
-        trigger="click"
-        overlay={
-          <Tooltip id="overlay">
-            <TtInner>
-                <div>Care to anonymously rate this recipe?</div>
-                <FormRow>
-                  <FormGroup>
-                    <Label htmlFor="ease" content="Ease rating" />
-                    <Input
-                      type="number"
-                      id="ease"
-                      max="5"
-                      min="1"
-                      value={ease}
-                      onChange={(e) => setEase(e.target.value)}
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label htmlFor="taste" content="Taste rating" />
-                    <Input
-                      type="number"
-                      id="taste"
-                      value={taste}
-                      max="5"
-                      min="1"
-                      onChange={(e) => setTaste(e.target.value)}
-                    />
-                  </FormGroup>
-                </FormRow>
-                <PrimaryButton onClick={() => handleSubmit(ease, taste)}>
-                  Submit
-                </PrimaryButton>
-            </TtInner>
-          </Tooltip>
-        }
-        rootClose
-      >
+      <Button ref={target} onClick={() => setShow(!show)}>
         <Icon
           icon={faCheck}
           isactive={props.isactive}
           onClick={props.onClick}
         />
-      </OverlayTrigger>
+      </Button>
+      <Overlay target={target.current} show={show} placement="right">
+        {(props) => (
+          <Tooltip id="overlay" {...props}>
+            <TtInner>
+              <div>Care to anonymously rate this recipe?</div>
+              <FormRow>
+                <FormGroup>
+                  <Label htmlFor="ease" content="Ease rating" />
+                  <Input
+                    type="number"
+                    id="ease"
+                    max="5"
+                    min="1"
+                    value={ease}
+                    onChange={(e) => setEase(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label htmlFor="taste" content="Taste rating" />
+                  <Input
+                    type="number"
+                    id="taste"
+                    value={taste}
+                    max="5"
+                    min="1"
+                    onChange={(e) => setTaste(e.target.value)}
+                  />
+                </FormGroup>
+              </FormRow>
+              <PrimaryButton onClick={() => handleSubmit(ease, taste)}>
+                Submit
+              </PrimaryButton>
+            </TtInner>
+          </Tooltip>
+        )}
+      </Overlay>
     </>
   );
-};
+}
 
 const Check = ({ slug }) => {
   const [busy, setBusy] = useState(false);
@@ -128,6 +132,12 @@ const Check = ({ slug }) => {
   return (
     <div>
       <RateTT
+        slug={slug}
+        closeRate={() => setRate(false)}
+        isactive={isChecked ? 1 : 0}
+        onClick={onClick}
+      />
+      <RateToolTip
         slug={slug}
         closeRate={() => setRate(false)}
         isactive={isChecked ? 1 : 0}
